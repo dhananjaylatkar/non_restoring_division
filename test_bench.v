d526430f0f1f1f5aa11e3835640e3b2a65b95c5d
NamrathaS <31969433+NamrathaS@users.noreply.github.com>
2017-09-15 10:28:00 +0530

    Update test_bench.v

--- test_bench.v
+++ test_bench.v
@@ -1,1 +1,25 @@
-// namratha will edit this file
+`include "non_restoring_divison.v"
+module non_restoring_divider_tb;
+//inputs
+reg[3:0] X;  
+reg[3:0] Y; 
+//outputs
+wire[4:0] R;
+wire[3:0] Q; 
+non_restoring_divider uut (.X(X),.Y(Y));
+initial begin
+//intialise inputs
+  X=4'b0000; Y=4'b0000;
+  //reset
+  #200 X=4'b0110; Y=4'b0010;
+  #200 X=4'b1100; Y=4'b0011; 
+  #200 X=4'b1101; Y=4'b1100; 
+  #200 X=4'b0101; Y=4'b1010; 
+  #200 X=4'b1001; Y=4'b1100; 
+  #200 X=4'b1110; Y=4'b1001; 
+end
+initial begin
+	$dumpfile("non_restoring_divison.vcd");
+	$dumpvars();
+end
+endmodule
